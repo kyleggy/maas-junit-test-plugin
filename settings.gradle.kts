346d89bbbc8c49e12126533401960c0024cbfc54
GitHub Action <action@github.com>
2021-05-12 11:04:59 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "maas-junit-test-plugin"
---
+++ src/main/kotlin/com/github/kyleggy/maasjunittestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kyleggy.maasjunittestplugin.services
+
+import com.github.kyleggy.maasjunittestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
